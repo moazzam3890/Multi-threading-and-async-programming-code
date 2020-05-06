2866308b2872b9925dee4c074890b0a963e7c771
Moazzam Adil Khan <moaadi3890@gmail.com>
2020-05-06 15:24:46 +0500

    trying to print final value

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ use futures::executor::block_on;
 use async_std::task;
 
 fn main() {
-    result = block_on(async_main());
-    println!("{}",result);
+    println!("{}", block_on(async_main()));
 }
 
 async fn async_main() -> i32{
-    let neg = neg_async(1);
-    println!("{}", neg_async());
-    let negating = task::spawn(neg_async(2));
-    neg.await + negating.await
+    let neg = neg_async(1).await;
+    println!("{}", neg);
+    let negating = task::spawn(neg_async(2)).await;
+    println!("{}", negating);
+    neg + negating    
+}
+
+async fn neg_async(f: i32) -> i32 {
+    println!("negating for : {}", f);
+    task::sleep(std::time::Duration::from_secs(3)).await;
+    println!("Finished sleeping for 3 seconds");
+    f * -1
 }
 
